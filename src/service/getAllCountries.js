0428f99f4e44bacc059db56d947599daac08dcd4
Franckie <franckie.dev@gmail.com>
2025-06-12 16:07:10 +0300

    fix(api): add required fields param to "/all" endpoint

--- src/service/getAllCountries.js
+++ src/service/getAllCountries.js
@@ -1,6 +1,6 @@
 import { api } from "./api.js"
 
 export const getAllCountries = async () => {
-    const response = await api.get('/all')
+    const response = await api.get('/all?fields=name,cca2,capital,region,population,flags,languages,currencies,borders,subregion')
     return response.data
 }
